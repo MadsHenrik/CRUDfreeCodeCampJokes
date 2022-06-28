f6c25196c69fb43dbb8f8d634788afb5af7c9733
MadsHenrik <mh.tonheim@student.uis.no>
2022-06-28 23:01:46 +0200

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRUDfreeCodeCampJokes.Models;
+
+namespace CRUDfreeCodeCampJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CRUDfreeCodeCampJokes.Models.Joke> Joke { get; set; }
+    }
+}
